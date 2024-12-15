0be733c2e8be3076cd48af48e3d3ca094f0c182d
movin-yectra <movin@yectra.com>
2024-12-15 18:22:47 +0530

    fix

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,4 +1,5 @@
-import myImage from '../assets/d8d6d0061b327465a7989ebb430191788f41014bce397c0e8650de6ed21ac6ad.png';
+import { motion } from 'framer-motion';
+import myImage from '../assets/WhatsApp Image 2024-12-12 at 13.47.27_30755d03.jpg';
 import resume from '../assets/MovinResume.pdf';
 
 const Banner = () => {
@@ -11,24 +12,35 @@ const Banner = () => {
     document.body.removeChild(link);
   };
   return (
-    <div className="w-full text-center">
-      <img
-        src={myImage}
-        alt="profile picture"
-        className="object-contain mt-14 aspect-[1.13] w-[201px] m-auto"
+    <motion.div
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 1, ease: 'easeOut' }}
+      className="w-full text-center"
+    >
+      <motion.div
+        className="mt-10 w-[201px] h-[201px] m-auto rounded-full overflow-hidden bg-cover bg-center transition-all duration-300 cursor-pointer"
+        style={{
+          backgroundImage: `url(${myImage})`,
+          backgroundPosition: 'top center', // Adjust as needed (e.g., 'top left', 'bottom right')
+        }}
+        initial={{ scale: 0.8 }}
+        whileHover={{ scale: 1.1 }} // Zoom in to 120% on hover
+        transition={{ duration: 0.1, ease: 'backInOut' }}
       />
-      <div className="mt-10 text-6xl font-extrabold  text-white bg-clip-text bg-[linear-gradient(116deg,#FF8660_71.79%,#9A33FF_95.51%)] leading-[63px] max-md:mt-10 max-md:max-w-full max-md:text-4xl max-md:leading-[50px]">
-        <span className="text-white">Turning Coffee into Code </span>
+      <div className="mt-5 text-5xl font-extrabold text-white bg-clip-text bg-[linear-gradient(116deg,#FF8660_71.79%,#9A33FF_95.51%)] leading-[63px] max-md:mt-5 max-md:max-w-full max-md:text-4xl max-md:leading-[50px]">
+        <span className="text-white">Turning Coffee into Code</span>
       </div>
-      <div className="mt-3 text-3xl font-extrabold  text-white bg-clip-text bg-[linear-gradient(116deg,#FF8660_71.79%,#9A33FF_95.51%)] leading-[63px] max-md:mt-10 max-md:max-w-full max-md:text-4xl max-md:leading-[50px]">
+
+      <div className="mb-3 text-3xl font-extrabold  text-white bg-clip-text bg-[linear-gradient(116deg,#FF8660_71.79%,#9A33FF_95.51%)] leading-[63px] max-md:mt-5 max-md:max-w-full max-md:text-3xl max-md:leading-[30px]">
         <span className="text-white">React Websites That Just Click! </span>
       </div>
-      <div className="mx-auto mt-10 text-lg font-light tracking-wide w-[650px] text-stone-300 max-md:max-w-72 ">
+      <div className="mx-auto text-lg font-light tracking-wide w-[650px] text-stone-300 max-md:max-w-72 ">
         I am a skilled React developer with 3+ years of experience, specializing in building dynamic
         front-end applications. My expertise is in creating responsive, user-focused interfaces that
         are both scalable and efficient...
       </div>
-      <div className="md:flex md:gap-5 mt-14  items-center text-xl font-semibold md:w-[417px] max-md:mt-10 m-auto">
+      <div className="md:flex md:gap-5 mt-10  items-center text-xl font-semibold md:w-[417px] max-md:mt-10 m-auto">
         <div className="px-8 cursor-pointer hover:bg-black hover:text-white py-4 bg-white border border-white border-solid rounded-[50px] text-neutral-900 max-md:px-5">
           <a target="_blank" href="https://www.linkedin.com/in/movin-d-a64ab212a/" className="">
             Get In Touch
@@ -41,7 +53,7 @@ const Banner = () => {
           Download CV
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
